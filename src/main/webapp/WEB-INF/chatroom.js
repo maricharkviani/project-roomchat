f7fb3e9c3c822aae76cf17a94df289593bdb5af8
maricharkviani <mariamcharkviani2007@gmail.com>
2024-04-08 18:26:32 +0400

    Initial commit

---
+++ src/main/webapp/WEB-INF/chatroom.js
@@ -0,0 +1,6 @@
+const roomName = document.getElementById("room-name");
+const messages = document.getElementById("messages");
+const messageForm = document.getElementById("message-form");
+const messageInput = document.getElementById("message-input");
+
+// Code to fetch and
---
+++ src/main/webapp/WEB-INF/index.js
@@ -0,0 +1,8 @@
+const chatRooms = document.getElementById("chat-rooms");
+const createRoomBtn = document.getElementById("create-room-btn");
+
+// Code to fetch and display chat rooms goes here
+
+createRoomBtn.addEventListener("click", () => {
+    // Code to display the window to create a new room goes here
+});
